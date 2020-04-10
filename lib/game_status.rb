744817dec56bd9b9caf0ef9749e9daeb86427447
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-10 22:51:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,6 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
+<<<<<<< HEAD
   WIN_COMBINATIONS.detect do |win_combo_array|
       board[win_combo_array[0]] == board[win_combo_array[1]] &&  board[win_combo_array[1]] == board[win_combo_array[2]] && position_taken?(board, win_combo_array[0]) 
   end
@@ -20,3 +21,25 @@ end
 
 
   
+=======
+  WIN_COMBINATIONS.each do |first_row_array|
+    win_index_1 = first_row_array[0]
+    win_index_2 = first_row_array[1]
+    win_index_3 = first_row_array[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_taken?(board, win_index_1) == false || position_taken?(board, win_index_2) == false || position_taken?(board, win_index_3) == false
+      false
+    else
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return first_row_array
+      else
+        false
+      end
+    end
+  end
+end
+>>>>>>> 0af30216fa30e886c3328c7572e8bab6d4facffb
