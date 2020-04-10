6cb031fcd9830f87c726c9390c1e170b1d8963d8
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-10 22:53:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,10 +7,10 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-<<<<<<< HEAD
   WIN_COMBINATIONS.detect do |win_combo_array|
       board[win_combo_array[0]] == board[win_combo_array[1]] &&  board[win_combo_array[1]] == board[win_combo_array[2]] && position_taken?(board, win_combo_array[0]) 
   end
+  return false
 end
 
 def full?(board)
